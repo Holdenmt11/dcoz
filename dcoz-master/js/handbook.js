a9f5c2f228518579fdee7496b5b0f9e7af84ab28
Holdenmt11 <holdenmt11@gmail.com>
2016-04-10 19:23:58 -0400

    Added files via upload

---
+++ dcoz-master/js/handbook.js
@@ -0,0 +1,3 @@
+
+document.getElementById("foot01").innerHTML =
+"<div style='background-color:#1C64B7; color:white; text-align:center; padding-bottom:20px; padding-top:20px'>&copy; 2016 District of Columbia Office of Zoning</div>";
