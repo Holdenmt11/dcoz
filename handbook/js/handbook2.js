d335e888426ad7e6bd2c48368f27d6355d29f51d
Holdenmt11 <holdenmt11@gmail.com>
2016-04-14 17:03:35 -0400

    Added files via upload

---
+++ handbook/js/handbook2.js
@@ -0,0 +1,211 @@
+document.getElementById("nav01").innerHTML =
+
+"<br>"+
+"<br>"+
+"<br>"+
+"<br>"+
+"<br>"+
+
+
+"<div class='container'>"+
+
+
+
+"<div class='navbar-inverse' role='navigation'>"+
+    "<div class='container-fluid'>"+
+        "<div class='navbar-header' >"+
+            "<button type='button' class='navbar-toggle' data-toggle='collapse' data-target='.navbar-ex1-collapse'> "+ 
+                "<span class='sr-only'>Toggle navigation</span>"+
+                "<span ></span>"+
+                "<span >Menu</span>"+
+                "<span ></span>"+
+            "</button>"+
+        "</div>"+
+        "<div class='collapse navbar-collapse navbar-ex1-collapse'>"+
+		
+            "<ul class='navbar-nav' >"+
+                "<li>"+
+                    "<a href='#' >DCOZ Home</a> "+
+               " </li>"+
+				
+				"<li class='dropdown'>"+
+                   " <a href='#'  data-toggle='dropdown'>Online Services <b class='caret'></b></a>"+
+                   " <ul class='dropdown-menu' >"+
+                     "   <li class='  dropdown  '>"+
+                          "  <a href='#' >IZIS - File and Manage Cases Online</a></li>"+
+						"	<li class='  dropdown  '>"+
+                         "   <a href='#' >IZIS - ZC & BZA Case Records</a></li>"+
+						"	<li class='  dropdown  '>"+
+                         "   <a href='#' >Email Submission</a></li>"+
+						"	<li class='  dropdown  '>"+
+                         "   <a href='#' >Zoning Map</a></li>"+
+						"	<li class='  dropdown  '>"+
+                         "   <a href='#' >Live WebCast - ZC and BZA</a></li>"+
+						"	<li class='  dropdown  '>"+
+                         "   <a href='#' >Video-on-Demand</a></li>"+
+						"	<li class='  dropdown  '>"+
+                         "   <a href='#' >Compliance Review</a></li>"+
+						"	<li class='  dropdown  '>"+
+                         "   <a href='#' >Zoning Certifications</a>"+
+                     "   </li>"+
+						
+                "    </ul>"+
+            "    </li>"+
+			"	<li class='  dropdown'>"+
+            "        <a href='#' data-toggle='dropdown'>ZC <b class='caret'></b></a>"+
+             "       <ul class='dropdown-menu' >"+
+              "          <li class='  dropdown  '>"+
+              "              <a href='#' >General Procedures</a></li>"+
+				"			<li class='  dropdown  '>"+
+                "            <a href='#' >General Rules</a></li>"+
+				"			<li class='  dropdown  '>"+
+                 "           <a href='#' >General Rules</a></li>"+
+				"			<li class='  dropdown  '>"+
+                 "           <a href='#' >General Rules</a></li>"+
+				"			<li class='  dropdown  '>"+
+                 "           <a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+                        "</li>"+
+						
+                    "</ul>"+
+                "</li>"+
+				"<li class='  dropdown'>"+
+                    "<a href='#' data-toggle='dropdown'>BZA <b class='caret'></b></a>"+
+                    "<ul class='dropdown-menu' >"+
+                        "<li class='  dropdown  '>"+
+                            "<a href='#' >General Procedures</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+                        "</li>"+
+						
+                    "</ul>"+
+                "</li>"+
+				"<li class='  dropdown'>"+
+                    "<a href='#' data-toggle='dropdown'>ZR16 <b class='caret'></b></a>"+
+                    "<ul class='dropdown-menu' >"+
+                        "<li class='  dropdown  '>"+
+                            "<a href='#' >General Procedures</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                           " <a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+                        "</li>"+
+						
+                    "</ul>"+
+                "</li>"+
+				"<li class='  dropdown'>"+
+                    "<a href='#' data-toggle='dropdown'>Resources <b class='caret'></b></a>"+
+                    "<ul class='dropdown-menu' >"+
+                        "<li class='  dropdown  '>"+
+                            "<a href='#' >General Procedures</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                           " <a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                           " <a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+                        "</li>"+
+						
+                    "</ul>"+
+                "</li>"+
+				"<li class='  dropdown'>"+
+                    "<a href='#' data-toggle='dropdown'>Search <b class='caret'></b></a>"+
+                    "<ul class='dropdown-menu' >"+
+                        "<li class='  dropdown  '>"+
+                            "<a href='#' >General Procedures</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                            "<a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                           " <a href='#' >General Rules</a></li>"+
+                        "</li>"+
+						
+                    "</ul>"+
+               " </li>"+
+				"<li class='  dropdown'>"+
+                    "<a href='#' data-toggle='dropdown'>About DCOZ <b class='caret'></b></a>"+
+                   " <ul class='dropdown-menu' >"+
+                       " <li class='  dropdown  '>"+
+                           " <a href='#' >General Procedures</a></li>"+
+							"<li class='  dropdown  '>"+
+                           " <a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                           " <a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                           " <a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                           " <a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                           " <a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                           " <a href='#' >General Rules</a></li>"+
+							"<li class='  dropdown  '>"+
+                           " <a href='#' >General Rules</a></li>"+
+                       " </li>"+
+						
+                   " </ul>"+
+                
+				
+				
+							
+		"</ul>"+
+		"</div>"+
+		"</div>"+
+		"</div>"+
+
+
+"</div>"+
+
+
+
+
+
+"</div>";
+
+
+document.getElementById("foot01").innerHTML =
+"<div style='background-color:#1C64B7; color:white; text-align:center; padding-bottom:20px; padding-top:20px'>&copy; 2016 District of Columbia Office of Zoning</div>";
